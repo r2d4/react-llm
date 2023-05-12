3fb89696e024091873d8e5604dd8912910606616
Matt Rickard <m@rickard.email>
2023-05-12 13:46:42 -0700

    wip

--- packages/headless/dist/types/src/contexts/ModelProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { UseLLMParams, UseLLMResponse } from "../hooks/useLLM";
-interface ModelProviderProps {
-    children: React.ReactNode;
-    props?: UseLLMParams;
-}
-export declare const ModelProvider: React.FC<ModelProviderProps>;
-export declare const useLLM: () => UseLLMResponse;
-export {};
