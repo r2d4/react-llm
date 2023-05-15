aafbba62aa0fb39184c37e16d28e228e3c113112
Matt Rickard <m@rickard.email>
2023-05-14 20:27:12 -0700

    add readme to subdirectory, bump version

--- packages/retro-ui/src/app/layout.tsx
+++ packages/retro-ui/src/app/layout.tsx
@@ -1,8 +1,9 @@
+import Script from "next/script";
 import React from "react";
 import "../styles/globals.css";
 
 export const metadata = {
-  title: "Local LLM Chat",
+  title: "AI Instant Messenger",
   description: "Chat with an in-browser Vicuna LLM model",
 };
 
@@ -20,6 +21,19 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Script
+        id={"google-analytics"}
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
     </html>
   );
 }
